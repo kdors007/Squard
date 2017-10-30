61dee46b765ed3e5bd5cfa87f18e3dbdf27772b9
Kareem <reemy396@aol.com>
2017-10-30 17:55:50 -0400

    Seeded file for your use

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,54 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create a User first
+# Sport League
+sport = [
+	['NFL', 1],
+	['NBA', 1]
+]
+
+sport.each do |sport, id|
+	Sport.create(league: sport, user_id: id)
+end
+
+# NFL teams
+nfl_teams = [
+	["Arizona Cardinals", 1],
+	["Chicago Bears", 1],
+	["Green Bay Packers", 1],
+	["New York Giants", 1],
+	["Detroit Lions", 1],
+	["Washington Redskins", 1],
+	["Philadelphia Eagles", 1],
+	["Pittsburgh Steelers", 1],
+	["Los Angeles Rams", 1],
+	["San Francisco 49ers", 1],
+	["Cleveland Browns", 1],
+	["Indianapolis Colts", 1],
+	["Dallas Cowboys", 1],
+	["Kansas City Chiefs", 1],
+	["Los Angeles Chargers", 1],
+	["Denver Broncos", 1],
+	["New York Jets", 1],
+	["New England Patriots", 1],
+	["Oakland Raiders", 1],
+	["Tennessee Titans", 1],
+	["Buffalo Bills", 1],
+	["Minnesota Vikings", 1],
+	["Atlanta Falcons", 1],
+	["Miami Dolphins", 1],
+	["New Orleans Saints", 1],
+	["Cincinnati Bengals", 1],
+	["Seattle Seahawks", 1],
+	["Tampa Bay Buccaneers", 1],
+	["Carolina Panthers", 1],
+	["Jacksonville Jaguars", 1],
+	["Baltimore Ravens", 1],
+	["Houston Texans", 1]
+]
+
+nfl_teams.each do |name, id|
+	Team.create(name: name, sport_id: id)
+end
